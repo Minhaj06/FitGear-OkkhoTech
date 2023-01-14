5d130936797a6760d15f21a043e4fdb46520e97a
Minhaj06 <milonmdminhajkobir@gmail.com>
2023-01-14 20:21:14 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Home Banner Swiper Slide Starts Here
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+// Home Banner Swiper Slide Ends Here
