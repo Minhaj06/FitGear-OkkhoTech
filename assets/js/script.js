2e44217b905a026bf4811c2d2877e4e7f85a2dd1
Minhaj06 <milonmdminhajkobir@gmail.com>
2023-01-15 19:24:54 +0600

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,34 @@
+$(document).ready(function () {
+  // Toggle Sidebar
+  $(".menu-icon").click(function () {
+    $(".sidebar").toggleClass("show-sidebar");
+    $(".body-wrapper-freeze").toggleClass("d-block");
+  });
+
+  $(".body-wrapper-freeze").click(function () {
+    $(".sidebar").toggleClass("show-sidebar");
+    $(".body-wrapper-freeze").toggleClass("d-block");
+  });
+
+  // Toggle Cart Sider
+  $(".cart-sider-toggler").click(function () {
+    $(".cart-sider").toggleClass("show-cart-sider");
+    $(".sider-body-wrapper-freeze").toggleClass("d-block");
+  });
+
+  $(".sider-body-wrapper-freeze").click(function () {
+    $(".cart-sider").toggleClass("show-cart-sider");
+    $(".sider-body-wrapper-freeze").toggleClass("d-block");
+  });
+});
+
 // Home Banner Swiper Slide Starts Here
 var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
